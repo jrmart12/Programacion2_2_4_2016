e3fd6efaa399060cbb7b69e19ead2493069aa2a9
Carlos Gochez <cgotcha@gmail.com>
2016-07-25 11:22:21 -0600

    Introduccion a la recursion

---
+++ src/recursion/TestRecursion.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package recursion;
+
+/**
+ *
+ * @author Aula
+ */
+public class TestRecursion {
+    public static void main(String[] args) {
+        
+        System.out.println("Print(5): \n-------------");
+        FuncionesRecursivas.print(15);
+        
+        System.out.println("PrintMismo(5,3): \n-------------");
+        FuncionesRecursivas.printMismo(5, 5);
+    }
+}
